242f93e70fb7a2b65a82112aff97a2aa514f2178
Kristian Gade Kjelmann <kgk@adm.aau.dk>
2026-09-11 11:23:30 +0200

    add annotation and storing + logging

--- Pipelines/Screengetter/screengetter_main.py
+++ Pipelines/Screengetter/screengetter_main.py
@@ -10,6 +10,7 @@ import json
 from pathlib import Path
 
 import numpy as np
+import polars as pl
 
 # Internal
 from Shared_Functions.logger_functionality import *
@@ -31,7 +32,7 @@ INPUT_EVENTS_PATH = INPUT_DIR_DATA / "Data_Combination" / "combined_data_when_fe
 # Directories - internal output
 OUTPUT_DIR_INT = Path(".") / "Pipelines" / "Screengetter" / "Data"
 OUTPUT_FRAMEARRAYS = OUTPUT_DIR_INT / "Framearrays"
-OUTPUT_REFERENCE = OUTPUT_DIR_INT / "Reference2"
+OUTPUT_REFERENCE = OUTPUT_DIR_INT / "Reference"
 
 # Directories - global output
 OUTPUT_BASE = Path(".") / "Data" / "Screengetter"
@@ -46,7 +47,7 @@ VIDEO_RESOLUTIONS = OUTPUT_REFERENCE / "resolutions.json"
 REFERENCE_SET = OUTPUT_REFERENCE / "reference_lookup_tagged.json"
 
 # Filename schema
-SCREENREC_FILE_SCHEMA = r"Scene_{GROUP}_{TASK}.PRIMO-RA"
+SCREENREC_FILE_SCHEMA = r"Scene_{GROUP}_{TASK}.PRIMO[-_]RA"
 
 # SEED
 SEED_NO = 1789021234
@@ -85,48 +86,56 @@ def _get_filename(
 
     filename_stub = filename_schema.format(
         GROUP = group_name,
-        TASK = task
+        TASK = f"{int(task):02d}"
     )
-
     screenrec_filepath = next(screenrecs_dir.glob(f"{filename_stub}*"), None)
 
     return screenrec_filepath
 
+def _combine_entries(group, task, event_timestamps, annotated_frames):
+
+    entries = []
+
+    for timestamp, frame_data in dict(zip(event_timestamps, annotated_frames)).items():
+
+        for start, end, frame, tag in frame_data:
+            combined_entry = {
+                "group": group,
+                "task": task, 
+                "feeling_timestamp": timestamp,
+                "frame_start": start, 
+                "frame_end": end, 
+                "frame": frame,
+                "tag": tag
+            }
+        
+        entries.append(combined_entry)
+    
+    return entries
+
+
 def _store_framearrays(
-    framearrays_in_intervals,
-    group_name,
-    task, 
-    output_frames_maindir = OUTPUT_FRAMEARRAYS
+    processed_events: list[dict],
+    output_frames_dir: Path = OUTPUT_FRAMEARRAYS
     ):
 
-    output_frames_dir = output_frames_maindir / f"{group_name}_{task}"
-    output_frames_dir.mkdir(parents = True, exist_ok=True)
+    output_path_meta = output_frames_dir / "event_frames_meta.json"
 
-    output_path_meta = output_frames_dir / f"{group_name}_{task}_framearrays_meta.json"
-
-    meta_out = []
-
-    for interval_framearrays in framearrays_in_intervals:
+    for event_dict in processed_events:
         
-        event_start = min([framearray[0] for framearray in interval_framearrays]) # First tuple item is the start of interval. Lowest value corresponds to start of event.
-    
-        for start, end, framearray in interval_framearrays:
+        frame = event_dict.pop("frame")
 
-            output_path_framearray = output_frames_dir / f"{start}_{end}.npy"
+        frame_arary_output_name = f'{event_dict["group"]}-{event_dict["task"]}_{event_dict["frame_start"]}-{event_dict["frame_end"]}.npy'
+        frame_arary_output_path = output_frames_dir / frame_arary_output_name
 
-            np.save(output_path_framearray, framearray)
+        np.save(frame_arary_output_path, frame)
 
-            meta_entry = {
-                "event_start": event_start,
-                "interval_start": start, 
-                "interval_end": end,
-                "framearray_path": str(output_path_framearray)
-            }
-
-            meta_out.append(meta_entry)
+        event_dict.update({
+            "framearray_path": frame_arary_output_name
+        })
 
     with open(output_path_meta, "w", encoding = "utf-8") as output_file:
-        json.dump(meta_out, output_file, indent=2)
+        json.dump(processed_events, output_file, indent=2)
     
 
 ## MAIN FUNCTION ##
@@ -154,42 +163,73 @@ def main(input_data = None) -> None:
         return
 
     smallest_resolution = _read_smallest_resolution(VIDEO_RESOLUTIONS)
-    reference_lookup = json.load(reference_lookup)
-
+    
     if input_data is None:
         events_df = load_events(input_file = INPUT_EVENTS_PATH)
 
-    group_tasks = zip(events_df.col("group"), events_df.col("task"))
+    #group_tasks = dict(events_df.select("group", "task").iter_rows())
+    group_tasks = {"C4": "13"}
+
+    processed_events = []
 
     for group, task in group_tasks.items():
 
-        screenrec_filepath = _get_filename(group, task)
+        logger.info(f"Processing events for {group}-{task}")
 
-        event_timestamps_group_task = events_df.filter(
-            events_df.col("group") == group & 
-            events_df.col("task") == task
-        ).get_column("feeling_timestamp").to_list()
+        try:
 
-        framearrays_in_intervals = extract_framearrays(
-            screenrec_filepath,
-            event_timestamps_group_task,
-            freq_per_s = 1,
-            standardize_resolution = False
+            screenrec_filepath = _get_filename(group, task)
+            
+            logger.info(f"Recording found at {screenrec_filepath}")
+
+            event_timestamps_group_task = events_df.filter(
+                (pl.col("group") == group) & 
+                (pl.col("task") == task)
+            ).get_column("feeling_timestamp").to_list()
+
+            if len(event_timestamps_group_task) == 0:
+                logging.warning(f"No events found for {group}-{task}. Continuing...")
+                continue
+
+            logger.info(f"{len(event_timestamps_group_task)} loaded for file {screenrec_filepath}")
+
+            framearrays_in_intervals = extract_framearrays(
+                screenrec_filepath,
+                event_timestamps_group_task,
+                freq_per_s = 1,
+                standardize_resolution = False
+            )
+
+            logger.info("Framearrays extracted")
+
+            frames_annotated = annotate_frames_in_intervals(
+                framearrays_in_intervals,
+                reference_dir = OUTPUT_REFERENCE,
+                reference_set = REFERENCE_SET,
+                use_resolution = smallest_resolution
+            )
+
+            logger.info("Framearraays annotated")
+
+            combined_entries = _combine_entries(group, task, event_timestamps_group_task, frames_annotated)
+
+            logger.info(f"Processed {len(combined_entries)} frames for {group}-{task}")
+
+            processed_events.extend(combined_entries)
+
+        except Exception as e:
+            logger.error(f"Processing failed for {group}-{task} with error: \n {e}")
+
+    logger.info(f"Storing {len(processed_events)} tagged frames to {OUTPUT_FRAMEARRAYS}")
+
+    try:        
+        _store_framearrays(
+            processed_events,
+            OUTPUT_FRAMEARRAYS
         )
+    except Exception as e:
+        logger.error(f"Failed to store processed events to {OUTPUT_FRAMEARRAYS} with error: \n {e}")
 
-        frames_annotated = annotate_frames_in_intervals(
-            framearrays_in_intervals,
-            reference_dir = OUTPUT_REFERENCE,
-            reference_set = REFERENCE_SET
-        )
-
-        # TODO: Store framearrays + dataset of event, start, end, framepath, tag
-
-        #_store_framearrays(
-        #    framearrays_in_intervals, 
-        #    group, 
-        #    task
-        #)
 
         # TODO: Analyze tag function: last tag, change in tag
 
